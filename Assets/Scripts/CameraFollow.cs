433472b511442d426bfb09cbf472515a08c1a3b7
Yeray <ytarifa2001@gmail.com>
2022-11-18 00:13:56 +0100

    Camera follow (smooth)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
