36c33798f08a670d9fc4ab51168193e4933c72d5
Yeray <ytarifa2001@gmail.com>
2022-11-18 00:38:58 +0100

    Camera screen limits

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,16 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public Vector2 cameraLimit;
 
     public float smoothSpeed = 10f;
 
     void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
+
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, -cameraLimit.x, cameraLimit.x);
+        desiredPosition.z = Mathf.Clamp(desiredPosition.z, -cameraLimit.y, cameraLimit.y);
 
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
     }
