0d64d75d1f927833491ae51205c8c573ffcc4126
Bosco Barber Esbert <boscobarberesbert@gmail.com>
2022-11-19 15:10:41 +0100

    Enemy chase AI based on distance

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    public Transform player;
+    public GameObject closestPlayer;
+    public GameObject[] players;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,25 @@ public class EnemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(player.position);
+        // Fill the array with the active players (GameObjects with tag "Player").
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        // Setting the first player in the array as the closest one (as a starting point).
+        closestPlayer = players[0];
+        float closestPlayerDistance = Vector3.Distance(closestPlayer.transform.position, this.transform.position);
+        float currentPlayerDistance;
+
+        // Traverse the array in order to see which one is closer to the enemy.
+        for (int i = 0; i < players.Length; ++i)
+        {
+            currentPlayerDistance = Vector3.Distance(players[i].transform.position, this.transform.position);
+            if (currentPlayerDistance < closestPlayerDistance)
+            {
+                closestPlayer = players[i];
+                closestPlayerDistance = currentPlayerDistance;
+            }
+        }
+
+        enemy.SetDestination(closestPlayer.transform.position);
     }
 }
