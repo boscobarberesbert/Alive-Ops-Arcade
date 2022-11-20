9595071e5e8af0a34eebbe1ac61cc8d22b93ee04
Yeray <ytarifa2001@gmail.com>
2022-11-20 13:59:06 +0100

    Aim and shoot script refactor and bullet life

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime = 1f;
+
+    public void Setup(Vector3 shootDirection)
+    {
+        // Rotate bullet to shoot direction
+        transform.forward = shootDirection.normalized;
+
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            Destroy(collision.collider.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/MouseAim.cs
+++ Assets/Scripts/Player/MouseAim.cs
@@ -5,18 +5,41 @@ using UnityEngine.InputSystem;
 
 public class MouseAim : MonoBehaviour
 {
+    // Aim members //
+    public float rotationSpeed = 5f;
+    private Vector3 worldPosition;
+    private Vector3 aimDirection;
+    private Quaternion lookRotation;
     [SerializeField] private LayerMask mouseColliderLayerMask;
 
-    public float rotationSpeed = 5f;
+    // Shoot Members //
+    public GameObject bullet;
+    public float shootForce = 150f;     // Bullet force
+    public Transform muzzlePoint;       // Point from where it shoots
 
-    private Vector3 screenPosition;
-    private Vector3 worldPosition;
+    PlayerInput playerInput;
 
-    [HideInInspector] public Vector3 direction;
-    private Quaternion lookRotation;
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+
+        // Key Down
+        playerInput.ShootingControls.Shoot.started += OnShoot;
+    }
+
+    private void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton()) // If Shoot button has been pressed
+            Shoot();
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        HandleAim();
+    }
+
+    private void HandleAim()
     {
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         if (Physics.Raycast(ray, out RaycastHit raycastHit, Camera.main.farClipPlane, mouseColliderLayerMask))
@@ -25,15 +48,49 @@ public class MouseAim : MonoBehaviour
         }
 
         // Find vector pointing from the player position to the mouse world position
-        direction = (worldPosition - transform.position).normalized;
-        
+        aimDirection = (worldPosition - transform.position).normalized;
+
         // We want to only aim in the xz plane
-        direction.y = 0;
+        aimDirection.y = 0;
 
         // Perform rotation to look
-        lookRotation = Quaternion.LookRotation(direction);
+        lookRotation = Quaternion.LookRotation(aimDirection);
 
         // Rotate over time according to speed
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        Vector3 targetPos;
+
+        Ray ray = new Ray(muzzlePoint.position, aimDirection);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            targetPos = raycastHit.point;
+        }
+        else
+            targetPos = ray.GetPoint(75); // Just a point far away from the player (missed)
+
+        Vector3 shootDirection = targetPos - muzzlePoint.position;
+
+        // Instantiate bullet in gun point
+        GameObject currentBullet = Instantiate(bullet, muzzlePoint.position, Quaternion.identity);
+
+        // Set properties of the bullet we have shot
+        currentBullet.GetComponent<Bullet>().Setup(shootDirection);
+
+        // Impulses the bullet to shoot direction
+        currentBullet.GetComponent<Rigidbody>().AddForce(shootDirection.normalized * shootForce, ForceMode.Impulse);
+    }
+
+    private void OnEnable()
+    {
+        playerInput.ShootingControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.ShootingControls.Disable();
     }
 }
