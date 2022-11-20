bd10ec91ef1b9e46fb48b0114bcaa7e6925544bc
Yeray <ytarifa2001@gmail.com>
2022-11-20 23:05:54 +0100

    Update PlayerID.cs

--- Assets/Scripts/Player/PlayerID.cs
+++ Assets/Scripts/Player/PlayerID.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AliveOpsArcade.OdinSerializer;
 
 public class PlayerID : MonoBehaviour
 {
     public int playerId = -1;
+    
+    private PlayerState currentState;
+
+    public void Start()
+    {
+        currentState = new PlayerState();
+        currentState.playerID = playerId;
+    }
+
+    public void Update()
+    {
+        currentState.position = transform.position;
+        byte[] bytes = SerializationUtility.SerializeValue(currentState, DataFormat.JSON);
+
+        //NetworkingManager.Instance.networking.SendPacket(bytes, NetworkingManager.Instance.networking.)
+    }
 }
